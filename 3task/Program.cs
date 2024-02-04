245d5c69e2f28046b515ff98610cd1e97db94fda
Alex Besenev <Besenev96@gmail.com>
2024-02-04 13:20:16 +0500

    add 3 task

---
+++ 3task/Program.cs
@@ -0,0 +1,13 @@
+﻿double[] arr = {120, 101, 301, 85, 30, 101, 55, 41, 63, 65 };
+int i = 0;
+while (i < arr.Length)
+{
+    if (arr[i] % 2 == 0)
+    {
+        System.Console.Write($"{arr[i]} "); // {} для того чтобы вывел массив а не текст
+    }
+    i = i + 1;
+
+}
+// double a = 101;
+// System.Console.WriteLine(a%2);
